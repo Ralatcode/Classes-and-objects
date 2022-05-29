23cde82292b29de45c90578fc32bbab55618875f
RalatPC <ralatrado@gmail.com>
2022-05-29 09:19:47 +0100

    fixed my add new item to add it to the  track list

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 class Student:
-    # [assignment] Skeleton class. Add your code here
+    # [assignment] Skeleton class. Add your code here\
+
+    # Constructor
     def __init__(self, name, age, tracks, score):
+        # creates instances 
         self.name = str(name)
         self.age = int(age)
         self.tracks = list(tracks)
@@ -11,12 +14,17 @@ class Student:
     def change_name(self, new_name):
         self.name = new_name
         print("The student has changed its name from ", self.name," to ", new_name)
+
+    # creates a change age method
     def change_age(self, new_age):
         self.name = int(new_age)
         print("The student age has changed from ", self.age, " to ", new_age)
+    # adds a new item to track method 
     def add_track(self, new_item):
-        self.tracks = new_item
-        print(new_item," was added as a new item")
+        self.tracks.append(new_item)
+        print(new_item," was added as a new item, new track is ", self.tracks)
+
+    # returns the score
     def get_score(self):
         return print("This is your score ", self.score)
     
